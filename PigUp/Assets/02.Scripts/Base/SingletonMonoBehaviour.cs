836d0d1314a13bfd15b5156b01c9e9a70d8bacc9
kkkkiiiiii <98201682+kkkkiiiiii@users.noreply.github.com>
2023-02-20 22:34:42 +0900

    CreateScene,Folder

---
+++ PigUp/Assets/02.Scripts/Base/SingletonMonoBehaviour.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            InitInstance();
+            return _instance;
+        }
+    }
+
+    private static void InitInstance()
+    {
+        if (_instance != null)
+        {
+            return;
+        }
+        else
+        {
+            _instance = GameObject.FindObjectOfType<T>();
+            if (_instance==null)
+            {
+                GameObject gameObject = new GameObject(typeof(T).Name);
+                _instance = gameObject.AddComponent<T>();
+            }
+        }
+    }
+}
