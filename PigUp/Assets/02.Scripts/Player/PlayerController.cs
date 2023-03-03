e7446c7f47d97e4c076c513e224da92036aef4c7
kkkkiiiiii <98201682+kkkkiiiiii@users.noreply.github.com>
2023-03-03 22:16:56 +0900

    PlayerController.cs 수정

--- PigUp/Assets/02.Scripts/Player/PlayerController.cs
+++ PigUp/Assets/02.Scripts/Player/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
 {
     public FloatingJoystick floatingJoystick;
     public float speed=1;
+    private bool isRunning;
     private Rigidbody _rigidbody;
     private Animator _animator;
     private Vector3 _move;
@@ -15,6 +16,7 @@ public class PlayerController : MonoBehaviour
     {
         _rigidbody = GetComponent<Rigidbody>();
         _animator = GetComponent<Animator>();
+        isRunning = false;
     }
 
     private void FixedUpdate()
@@ -29,12 +31,27 @@ public class PlayerController : MonoBehaviour
         float z = floatingJoystick.Vertical;
         // 2. Move Position
         _move = new Vector3(x, 0, z) * speed * Time.fixedDeltaTime;
-        _rigidbody.MovePosition(_rigidbody.position + _move);
         if (_move.sqrMagnitude == 0)
+        {
+            _animator.SetBool("IsRunning", false);
             return;
-        // 3. Move Rotation
-        Quaternion dirQuaternion = Quaternion.LookRotation(_move);
-        Quaternion moveQuaternion = Quaternion.Slerp(_rigidbody.rotation,dirQuaternion,0.1f);
-        _rigidbody.MoveRotation(moveQuaternion);
+        }
+        if (_move.sqrMagnitude != 0)
+        {
+            isRunning = true;
+            _animator.SetBool("IsRunning", true);
+            _rigidbody.MovePosition(_rigidbody.position + _move);
+            // 3. Move Rotation
+            Quaternion dirQuaternion = Quaternion.LookRotation(_move);
+            Quaternion moveQuaternion = Quaternion.Slerp(_rigidbody.rotation,dirQuaternion,0.1f);
+            _rigidbody.MoveRotation(moveQuaternion);
+        
+            _animator.SetFloat("Blend",_move.sqrMagnitude);
+        }
+    }
+
+    void checkIsRunning()
+    {
+        
     }
 }
